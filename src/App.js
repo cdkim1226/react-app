a4c29ba18477b379032d86fee1fb55ab67d6ab06
김창대 <cdkim1226@gmail.com>
2021-05-27 04:12:26 +0900

    Create Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
+
+class Contents extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!!
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <Contents></Contents>
+        <Footer title="HTML" desc="HTML is HyperTest Markup Language."></Footer>
       </div>
     );
   }
